4f54eac0e27c1c25b5baff18cd8dbb5ae2971356
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2024-12-27 17:08:20 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,6 +5,7 @@ load_dotenv(find_dotenv())
 
 
 class Credentials:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
-    API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
-    API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8160540534:AAF8mZO0c9xkBYKsHRQavE2ap9eq8G3x-ug")# from @botfather
+    API_ID = int(os.getenv("API_ID", "28338127"))  # from https://my.telegram.org/apps
+    API_HASH = os.getenv("API_HASH", "c4cb64de16a18f8685a31716a0e2480e")  # from https://my.telegram.org/apps
+   
